57eed38ad731c90136573a1cc5586c244ed61214
SoniaCWeb <sonia.choudhury@hotmail.co.uk>
2020-02-24 19:08:27 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# todo
+
+`todo` is a basic React app I created to familiarise myself with basic React components. It's a productivity app, designed to create your own `todo` list. The user can creat, add and delete items on and off the list.
+
+# Duration
+
+1 day 
+
+# Technologies
+
+- React.js
+- JSX
+- CSS
+
+# UI and Design: 
+
+<img src="https://i.imgur.com/YjlfWpB.png" width="500px">
+<img src="https://i.imgur.com/QK3eEgk.png" width="500px">
---
+++ src/App.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import './style.css'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state={
+      newItem:'',
+      list: []
+    }
+  }
+
+  //implementing localstorage
+updateInput(key, value) {
+  //update react state 
+  this.setState({ 
+    [key]: value
+  })
+}
+
+  addItem(){ 
+    //create item with unique id
+    const newItem=  {
+      //math.random used so we get a random unique id everytime
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice()
+    }
+    //taking the current copy of list of items, i'll use the spread operator to to take the current copy of the list items 
+    const list= [...this.state.list]
+
+    //add the new item onto the list
+    list.push(newItem) 
+
+    //update state with new list and reset newItem input
+
+this.setState({
+  list, 
+  newItem:''
+    })      
+  }
+
+  deleteItem(id){
+    console.log('test123')
+    //copy current list of items
+   const list= [...this.state.list]
+  //filter out item being deleted
+   const updatedList = list.filter(item => item.id !== id)
+
+   this.setState({list: updatedList})
+  }
+
+  render () {
+   return (
+    <div className="App">
+
+     <div className="floating">
+        <div className="tossing"> 
+      <h1 className="app-title"> todo </h1>
+      </div>
+      </div>
+      
+     <div className="container">
+       <h1 className="app-subtitle">  Add An Item..
+         </h1> 
+       <br/>
+       <input className="input"
+       type="text"
+       placeholder="Type your item in here.."
+       value={this.state.newItem}
+       onChange={e => this.updateInput("newItem", e.target.value)}
+       />
+       <button  className="add-btn"
+       //onclick will call a function everytime the add button is clicked
+       onClick={e => this.addItem()}
+       >
+         Add
+         </button>
+         <br/>
+         <ul>
+           {this.state.list.map(item => {
+             return(
+               <li key={item.id}>
+                  {item.value}
+                  <button className="btn"
+                  onClick={() => this.deleteItem(item.id)}
+                  >
+                    Delete
+                  </button>
+               </li>
+             )
+           })}
+         </ul>
+     </div>
+    </div>
+  )
+   }
+}
+
+export default App;
